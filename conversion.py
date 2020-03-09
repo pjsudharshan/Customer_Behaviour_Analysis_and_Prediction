376f7d7b0f840ffe8eb93864feed5ce548a16add
Sudharshan P J <pjsudharshan@gmail.com>
2020-03-09 20:00:04 +0000

    py added

---
+++ conversion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: pjsudharshan
+"""
+####convert login data to usable data####
+
+import sys
+
+infile = sys.argv[1]
+tcomp = -1
+with open("company_log.csv","a") as g:
+    with open(infile) as f:
+        for line in f:
+            fields = line.strip().split()
+            comp = fields[1]
+            if comp!= tcomp:
+                tcomp = comp
+                g.write("\n{},{}".format(tcomp,fields[4]))
+            else:
+                g.write(",{}".format(fields[4]))
